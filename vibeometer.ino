af5e9239c9f1488ad718e0f6d731f4c81a2c9988
adarthur <adarthur@ncsu.edu>
2014-05-22 11:15:49 -0400

    Update vibeometer.ino
    Moved code from loop() into separate functions

--- vibeometer.ino
+++ vibeometer.ino
@@ -19,6 +19,8 @@
 #define BADPIN 6 // pin for the "unhappy tweet" indicator
 #define MENTIONPIN 5 // pin for the "thanks for the mention" indicator
 
+long currentMillis;
+
 // pins for shift register
 const int latchPin = 8;
 const int clockPin = 3;
@@ -28,7 +30,6 @@ const int dataPin = 2;
 const int ledPins[NUMLEDS] = {GOODPIN, BADPIN, MENTIONPIN}; // put the pins in an array for easy access
 double ledFreqs[NUMLEDS]; // frequencies at which LEDs are to blink
 double tempFreqs[NUMLEDS]; // temporary storage for frequency values
-int mentioned[NUMLEDS]; // is there a new mention? order is good mention, bad mention, regular mention
 
 //tweet stuff
 int numMentions = 0; // number of mentions (displayed on 7-seg)
@@ -36,6 +37,7 @@ int lastMentions = 0; // a temp variable for the above
 int tweetCount[3]; // counters for each type of tweet; format is {good tweets, bad tweets, mentions}
 int lastGoodTweets = 0; // temp variable for tweetCount[0]
 int lastBadTweets = 0; // temp variable for tweetCount[1]
+int mentioned[NUMLEDS]; // is there a new mention? order is good mention, bad mention, regular mention
 
 char ssid[] = "ncsu"; // network SSID (ncsu doesn't require a password but does require registration with NOMAD
 String currentLine = ""; // holder for parsing output from web page
@@ -100,143 +102,14 @@ void setup() {
 }
 
 void loop() {
-  int i, j, k;
-  long currentMillis = millis();
-  int freqTest;
+  currentMillis = millis();
+  
   updateLEDs();
-  //Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    updateLEDs();
-    //Serial.print(millis());
-    //updateLEDs();
-    //Serial.println("connected to server");
-    //updateLEDs();
-    // Make a HTTP request:
-    client.print("GET ");
-    updateLEDs();
-    client.print("/display/techshowcase/getTweets.php");
-    updateLEDs();
-    client.println(" HTTP/1.1");
-    updateLEDs();
-    client.println("Host:webdev.lib.ncsu.edu");
-    updateLEDs();
-    client.println("Connection: close");
-    updateLEDs();
-    client.println();
-    updateLEDs();
-  }
-
-  //Wait for incoming bits, and then put the 0s and 1s into currentLine
-  while(!client.available()){
-    updateLEDs();
-  }
-
-  String currentLine = "";
-  i = 0;
-  char charBuff[10];
-  
-  if(client.find("\r\n\r\n")){ // skip the header of the message
-    while (client.available()) {               // the data we're reading is in the format "g:b:m_nnn"
-      char c = client.read();                  // g is the numberof good tweets, b the number of bad tweets,
-      if(c == ':') {                           // and m the number of mentions. These influence the mood panels.
-        currentLine.toCharArray(charBuff, 10); // n1, n2, and n3 are 1 if there is a new good tweet mentioning @vibeometer,
-        tweetCount[i] = atoi(charBuff);        // bad tweet mentioning it, or just a new mention, respectively.
-        currentLine = "";
-        i++;
-      }
-      else if(c == '_') {
-        currentLine.toCharArray(charBuff, 10);
-        tweetCount[i] = atoi(charBuff);
-        for(i = 0; i < 3; i++) {
-          mentioned[i] = client.read();
-        }
-      }
-      else currentLine += c;      
-      updateLEDs();
-      
-    }
-
-    client.flush(); //get rid of anything in the buffer
-  }
-
+  connectToServer();
+  parseWebPage();
   updateLEDs();
-  
-  // translate ASCII to decimal
-  for(i = 0; i < 3; i++) {
-    if(mentioned[i] == 48) mentioned[i] = 0;
-    if(mentioned[i] == 49) mentioned[i] = 1;
-    updateLEDs();
-  }
-
-  // if the server's disconnected, stop the client (debugging)
-  /*if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    updateLEDs();
-  }*/
-  
-  // set the frequencies of the flashing of the good and bad mood panels
-  double totalTweets = tweetCount[0] + tweetCount[1];
-  ledFreqs[0] = MAXFREQ * (tweetCount[0] / totalTweets);
-  ledFreqs[1] = MAXFREQ * (tweetCount[1] / totalTweets); 
-  
-  /*
-   * handle outputs regarding mentions of @vibeometer
-   */
-   
-  numMentions = tweetCount[2];
-  
-  // flash the "thanks for the @mention" panel if there is a new mention of @vibeometer
-  if(numMentions > lastMentions) {
-    ledFreqs[2] = MAXFREQ;
-    //Serial.println("new mention");
-  }
-  else ledFreqs[2] = 0;
-  
-  // store the frequency values so they can be restored later if the frequency is changed below
-  tempFreqs[0] = ledFreqs[0];
-  tempFreqs[1] = ledFreqs[1];
-  
-  // if there is a new good tweet, flash the good mood panel at 8 Hz
-  if(tweetCount[0] > lastGoodTweets) {
-    ledFreqs[0] = 8;
-    //Serial.println("new good tweet");
-  }
-  // same thing for a bad tweet
-  if(tweetCount[1] > lastBadTweets) {
-    ledFreqs[1] = 8;
-    //Serial.println("new bad tweet");
-  }
-  
-  if(numMentions >= 100) numMentions = 99; // make sure the count never goes above two digits
-  
-  lastMentions = numMentions;
-  lastGoodTweets = tweetCount[0];
-  lastBadTweets = tweetCount[1];
-  
-  updateCounter(numMentions);
-
-  // let the mention panel flash for 3 seconds
-  for(currentMillis = millis(); millis() - currentMillis < 3000; ) { 
-    updateLEDs();
-  }
-  
-  ledFreqs[2] = 0;
-  
-  // if a mood panel was just updated, let it flash for 1.5 more seconds
-  for(currentMillis = millis(); millis() - currentMillis < 1500; ) { 
-    updateLEDs();
-  }
-
-  // let the mood panels flash at the proper frequency again
-  ledFreqs[0] = tempFreqs[0];
-  ledFreqs[1] = tempFreqs[1];
-  
-  // wait 6.5 more seconds before starting again
-  for(currentMillis = millis(); millis() - currentMillis < 6500; ) { 
-    updateLEDs();
-  }
+	updateMoodFreqs();
+  outputToPanels();
 }
 
 void printWifiStatus() {
@@ -272,5 +145,130 @@ void updateLEDs() {
   int nowMillis = millis();
   for(int i = 0; i <= 2; i++) {
     analogWrite(ledPins[i],255*sin(PI*ledFreqs[i]*nowMillis/1000)*sin(PI*ledFreqs[i]*nowMillis/1000));
+  }
+}
+
+void connectToServer() {
+  //Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    updateLEDs();
+    //Serial.print(millis());
+    //updateLEDs();
+    //Serial.println("connected to server");
+    //updateLEDs();
+    // Make a HTTP request:
+    client.print("GET ");
+    updateLEDs();
+    client.print("/display/techshowcase/getTweets.php");
+    updateLEDs();
+    client.println(" HTTP/1.1");
+    updateLEDs();
+    client.println("Host:webdev.lib.ncsu.edu");
+    updateLEDs();
+    client.println("Connection: close");
+    updateLEDs();
+    client.println();
+    updateLEDs();
+  }
+
+  //Wait for incoming bits, and then put the 0s and 1s into currentLine
+  while(!client.available()){
+    updateLEDs();
+  }
+}
+
+// parse the text output by the PHP page
+void parseWebPage() {
+	String currentLine = "";
+  char charBuff[10];
+  int i = 0;	
+	
+  if(client.find("\r\n\r\n")){ // skip the header of the message
+    while (client.available()) {               // the data we're reading is in the format "g:b:m_nnn"
+      char c = client.read();                  // g is the numberof good tweets, b the number of bad tweets,
+      if(c == ':') {                           // and m the number of mentions. These influence the mood panels.
+        currentLine.toCharArray(charBuff, 10); // n1, n2, and n3 are 1 if there is a new good tweet mentioning @vibeometer,
+        tweetCount[i] = atoi(charBuff);        // bad tweet mentioning it, or just a new mention, respectively.
+        currentLine = "";
+        i++;
+      }
+      else if(c == '_') {
+        currentLine.toCharArray(charBuff, 10);
+        tweetCount[i] = atoi(charBuff);
+        for(i = 0; i < 3; i++) {
+          mentioned[i] = client.read();
+        }
+      }
+      else currentLine += c;      
+      updateLEDs();
+    }
+	
+	client.flush(); //get rid of anything in the buffer
+  }
+	
+	// translate ASCII to decimal
+  for(i = 0; i < 3; i++) {
+    if(mentioned[i] == 48) mentioned[i] = 0;
+    if(mentioned[i] == 49) mentioned[i] = 1;
+    updateLEDs();
+  }
+	
+	numMentions = tweetCount[2];
+}
+
+// set the frequencies of the flashing of the good and bad mood panels
+void updateMoodFreqs() {
+  double totalTweets = tweetCount[0] + tweetCount[1];
+  
+	ledFreqs[0] = MAXFREQ * (tweetCount[0] / totalTweets);
+  ledFreqs[1] = MAXFREQ * (tweetCount[1] / totalTweets); 
+}
+
+// handle any output to the flashing panels
+void outputToPanels() {
+	// store the frequency values so they can be restored later if the frequency is changed below
+  tempFreqs[0] = ledFreqs[0];
+  tempFreqs[1] = ledFreqs[1];
+  
+  // if there is a new good tweet, flash the good mood panel at 8 Hz
+  if(mentioned[0]) {
+    ledFreqs[0] = 8;
+  }
+	
+  // same thing for a bad tweet
+  if(mentioned[1]) {
+    ledFreqs[1] = 8;
+  }
+	
+	// flash the "thanks for the @mention" panel if there is a new mention of @vibeometer
+  if(numMentions > lastMentions) {
+    ledFreqs[2] = MAXFREQ;
+  }
+  else ledFreqs[2] = 0;
+  
+  if(numMentions >= 100) numMentions = 99; // make sure the count never goes above two digits
+  lastMentions = numMentions;  
+  updateCounter(numMentions);
+
+  // let the mention panel flash for 3 seconds
+  for(currentMillis = millis(); millis() - currentMillis < 3000; ) { 
+    updateLEDs();
+  }
+  
+  ledFreqs[2] = 0; // stop the flashing mention panel
+  
+  // let the flashing mood panel(s) go for 1.5 more seconds
+  for(currentMillis = millis(); millis() - currentMillis < 1500; ) { 
+    updateLEDs();
+  }
+
+  // let the mood panels flash at the proper frequency again
+  ledFreqs[0] = tempFreqs[0];
+  ledFreqs[1] = tempFreqs[1];
+  
+  // wait 6.5 more seconds before starting again
+  for(currentMillis = millis(); millis() - currentMillis < 6500; ) { 
+    updateLEDs();
   }
 }
